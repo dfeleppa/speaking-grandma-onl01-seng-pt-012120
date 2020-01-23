e65fd6fbfe3eab0a45a5cca237d0e14d6af3f93b
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-23 11:41:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   if phrase == phrase.downcase()
-    puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   elseif phrase = "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   else  
-    puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   end  
 end
 
