8fd06b23aefac8e3f2ca7b46123d2b071d29fc2c
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-23 11:44:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@ def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elseif phrase == phrase.upcase()
-    "HUH?! SPEAK UP, SONNY!"
-  else  
     "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
   end  
 end
 
